534b7df1a78df95144b5d7b4dad4e6e60f6b3bdc
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-03-28 14:37:04 +0300

    separate function to manage response errors

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,12 @@
 export default function fetchCountries(name) {
   const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
 
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return fetch(url).then(manageErrors);
+}
+
+function manageErrors(response) {
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+  return response.json();
 }
